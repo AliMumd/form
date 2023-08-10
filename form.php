7e10f67ea1cffad817726c8a28c0d4f660f81448
AliMumd <141897946+AliMumd@users.noreply.github.com>
2023-08-10 10:04:20 +0500

    Add files via upload

---
+++ form.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<form action="work.php" method="POST">
+<input type="text" name="name" placeholder="Enter your name"><br>
+<input type="email" name="email" placeholder="Enter your email"><br>
+<input type="text" name="age" placeholder="Enter your age"><br>
+<input type="text" name="address" placeholder="Enter your address"><br>
+<input type="text" name="phoneno" placeholder="Enter your phone no"><br>
+<input type="submit" value="submit">
+</form>
+
+</body>
+</html>
